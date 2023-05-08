0d0c54472610963cc178d0535d78ba5ae8c619b4
seovee <jinseob102@naver.com>
2023-05-08 17:40:18 +0900

    처음셋팅

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { useState, useEffect } from "react";
+
+function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
+  return (
+    <div>
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+    </div>
+  );
+}
+
+export default App;
